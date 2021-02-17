95d8037e094984915e43b36a42ffccbac96ae8ba
FlorianAinadou <florian.ainadou97@gmail.com>
2021-02-18 00:13:08 +0100

    Ajout des elements period dans le backend

--- backend/src/server/DataRegistration/sdk/DataFromHospital.js
+++ backend/src/server/DataRegistration/sdk/DataFromHospital.js
@@ -29,4 +29,44 @@ async function getDataFromHospital(){
     return await DataFromHospital.find();
 }
 
-export { addDataFromHospital, getDataFromHospital }
+async function getDataFromHospitalAfterDate(date){
+    const dateToCompare= Date.parse(date);
+    let dataAfterDate= [];
+    let data =  await DataFromHospital.find();
+    for (const dataFromHospital in data) {
+        if (Date.parse(dataFromHospital.jour)> dateToCompare){
+            dataAfterDate.push(dataFromHospital);
+        }
+    }
+
+    return dataAfterDate;
+}
+
+async function getDataFromHospitalBeforeDate(date){
+    const dateToCompare= Date.parse(date);
+    let dataBeforeDate= [];
+    let data =  await DataFromHospital.find();
+    for (const dataFromHospital in data) {
+        if (Date.parse(dataFromHospital.jour)< dateToCompare){
+            dataBeforeDate.push(dataFromHospital);
+        }
+    }
+
+    return dataBeforeDate;
+}
+
+async function getDataFromHospitalInPeriod(startDate, endDate){
+    const startDateToCompare= Date.parse(startDate);
+    const endDateToCompare= Date.parse(endDate);
+    let dataInPeriod= [];
+    let data =  await DataFromHospital.find();
+    for (const dataFromHospital in data) {
+        if (Date.parse(dataFromHospital.jour)< endDateToCompare && Date.parse(dataFromHospital.jour)> startDate){
+            dataInPeriod.push(dataFromHospital);
+        }
+    }
+
+    return dataInPeriod;
+}
+
+export { addDataFromHospital, getDataFromHospital, getDataFromHospitalBeforeDate, getDataFromHospitalInPeriod, getDataFromHospitalAfterDate }
