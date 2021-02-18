633c5c471e541209753a9bddbd68c58516393ed2
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2021-02-18 13:26:17 +0100

    #9 affichage des valeurs issues du fetch

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,13 +1,21 @@
-import axios from 'axios'
+// import axios from 'axios';
 
-const url = 'http://localhost:9000/api';
+// const url = 'http://localhost:9000/api/getTotalData';
 
 export const fetchTotalData = async () => {
-    let getTotalDataUrl = `${url}/getTotalData`;
-    try {
-        const totalData = await axios.get(getTotalDataUrl)
-        console.log(totalData)
-    } catch (error) {
-        console.log(error)
-    }
+    const totalData = [{
+        numberOfHospitalized: 10670743,
+        numberOfPeopleInRea: 1454888,
+        numberOfDeaths: 15347957,
+        numberOfRecovered: 64606679,
+        lastUpdateDate: '2021-01-31',
+        }];
+    // try {
+    //     totalData = await axios.get(url)
+    //     console.log(totalData)
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    // console.log(totalData)
+    return totalData;
 }
--- frontend/src/pages/CovidTracker.js
+++ frontend/src/pages/CovidTracker.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const CovidTracker = () => (
-    <div>
-        <h1> Hello Covid tracker</h1>
-    </div>
-)
+import { fetchTotalData } from '../api';
+
+export const CovidTracker = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setData(await fetchTotalData());
+            // console.log(data);
+        }
+        fetchAPI();
+        // console.log(data);
+    }, []);
+
+    return (
+        <div>
+            <h1> Hello Covid tracker</h1>
+            <h2>{ data.map(({ numberOfHospitalized }) => numberOfHospitalized) }</h2>
+        </div>
+    );
+}
+
