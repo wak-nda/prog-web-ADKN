c9013cf3f7a1cd01175eaf9a0d6f2830ac219d49
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2021-02-21 21:57:54 +0100

    #12 correction des erreurs linter

--- frontend/src/components/Chart/Chart.js
+++ frontend/src/components/Chart/Chart.js
@@ -15,63 +15,65 @@ export const Chart = ({ data: { totalConfirmed, totalRecovered, totalDeaths }, d
         fetchAPI();
     }, []);
 
+    const barChartData = {
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [{
+            label: 'People',
+            backgroundColor: [
+                'rgba(0,0,255,0.5)',
+                'rgba(0,255,0,0.5)',
+                'rgba(255,0,0,0.5)'
+            ],
+            data: [totalConfirmed, totalRecovered, totalDeaths]
+        }]
+    };
+
+    const barChartOptions = {
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${departement}` },
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+
+    const lineChartData = {
+        labels: monthlyData.map(({ date }) => date),
+        datasets: [{
+            data: monthlyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true
+        }, {
+            data: monthlyData.map(({ recovered }) => recovered),
+            label: 'Recovered',
+            borderColor: 'green',
+            backgroundColor: 'rgba(0,255,0,0.5)',
+            fill: true
+        }, {
+            data: monthlyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255,0,0,0.5)',
+            fill: true
+        }]
+    }
+
     const lineChart = (
         monthlyData.length
         ? (
-            <Line data={{
-                labels: monthlyData.map(({ date }) => date),
-                datasets: [{
-                    data: monthlyData.map(({ confirmed }) => confirmed),
-                    label: 'Infected',
-                    borderColor: '#3333ff',
-                    fill: true
-                }, {
-                    data: monthlyData.map(({ recovered }) => recovered),
-                    label: 'Recovered',
-                    borderColor: 'green',
-                    backgroundColor: 'rgba(0,255,0,0.5)',
-                    fill: true
-                }, {
-                    data: monthlyData.map(({ deaths }) => deaths),
-                    label: 'Deaths',
-                    borderColor: 'red',
-                    backgroundColor: 'rgba(255,0,0,0.5)',
-                    fill: true
-                }]
-                }}
-            />
+            <Line data={lineChartData} />
         ) : null
     );
 
     const barChart = (
         totalConfirmed
         ? (
-            <Bar
-                data={{
-                    labels: ['Infected', 'Recovered', 'Deaths'],
-                    datasets: [{
-                        label: 'People',
-                        backgroundColor: [
-                            'rgba(0,0,255,0.5)',
-                            'rgba(0,255,0,0.5)',
-                            'rgba(255,0,0,0.5)'
-                        ],
-                        data: [totalConfirmed, totalRecovered, totalDeaths]
-                    }]
-                }}
-                options={{
-                    legend: { display: false },
-                    title: { display: true, text: `Current state in ${departement}` },
-                    responsive: true,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }}
-            />
+            <Bar data={barChartData} options={barChartOptions} />
         ) : null
     )
 
