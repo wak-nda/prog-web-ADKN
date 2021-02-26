b5fbbcfd42e2634b5eda19813b7b94437762d32c
djotihamNABAGOU <djotiham.nabagou@etu.unice.fr>
2021-02-26 04:48:31 +0100

    #6 implémentation de la map

---
+++ frontend/src/components/MapComponent.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Map, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './MapComponent.scss';
+
+export const MapComponent = (countries) => {
+	// console.log(countries);
+	const mapStyle = {
+		fillColor: 'white',
+		weight: 1,
+		color: 'black',
+		fillOpacity: 1
+	};
+
+	const onEachCountry = (country, layer) => {
+		layer.options.fillColor = country.properties.color;
+		const name = country.properties.ADMIN;
+		const confirmedText = country.properties.CONFIRMEDTEXT;
+		// console.log(name, confirmedText);
+		layer.bindPopup(`${name} ${confirmedText}`);
+	};
+
+	return (
+		<Map style={{ height: '90vh' }} zoom={2} center={[20, 100]}>
+			<GeoJSON
+				style={mapStyle}
+				data={countries.countries}
+				onEachFeature={onEachCountry}
+			/>
+		</Map>
+	);
+};
--- frontend/src/pages/Covid-19Map.js
+++ frontend/src/pages/Covid-19Map.js
@@ -1,8 +1,33 @@
-// import React, { /*useState, useEffect*/ } from 'react';
-// { Col, Container, Row } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Loading } from '../components/Loading';
+import { MapComponent } from '../components/MapComponent';
+import { Legend } from '../components/Legend';
+import { LoadCountriesTask } from '../tasks/LoadCountriesTask';
+import { legendItems } from '../entities/LegendItems'
 
-export const Covid19Map = () => (
+export const Covid19Map = () => {
+	const [countries, setCountries] = useState([]);
+	const legendItemsReverse = [...legendItems].reverse();
+	// console.log(legendItemsReverse);
+
+	const load = () => {
+		// console.log("load");
+		const loadCountriesTask = new LoadCountriesTask();
+		loadCountriesTask.load(setCountries);
+	};
+
+	useEffect(load, []);
+
+	return (
 		<div>
-			<h1>Map in progress...</h1>
+			{countries.length === 0 ? (
+				<Loading />
+			) : (
+				<div>
+					<MapComponent countries={countries} />
+					<Legend legendItems={legendItemsReverse} />
+				</div>
+			)}
 		</div>
 	);
+};
