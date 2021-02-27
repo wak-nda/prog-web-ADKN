6930b93fc7860b0c7b727abe1a9ef1185882e42f
djotihamNABAGOU <djotiham.nabagou@etu.unice.fr>
2021-02-27 23:51:42 +0100

    #6 map layer

--- frontend/src/components/ChartsFrance.js
+++ frontend/src/components/ChartsFrance.js
@@ -1,55 +1,55 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
 import PropTypes from 'prop-types';
 
+import { Line } from 'react-chartjs-2';
 import styles from '../styles/css/ChartsFrance.module.scss';
 
 export const ChartsFrance = ({ dailyDataFrance }) => {
 	console.log(dailyDataFrance);
-    const lineChartCasData = {
-        labels: dailyDataFrance.map(({ date }) => date),
-        datasets: [{
-            data: dailyDataFrance.map(({ casConfirmes }) => casConfirmes),
-            label: 'Infectés',
-            borderColor: '#3333ff',
-            fill: true,
-            pointRadius: 0
-        }]
-    }
+	const lineChartCasData = {
+		labels: dailyDataFrance.map(({ date }) => date),
+		datasets: [{
+			data: dailyDataFrance.map(({ casConfirmes }) => casConfirmes),
+			label: 'Infectés',
+			borderColor: '#3333ff',
+			fill: true,
+			pointRadius: 0
+		}]
+	};
 
-    const lineChartCas = (
-        dailyDataFrance.length
-        ? (
-            <Line data={lineChartCasData} />
-        ) : null
-    );
+	const lineChartCas = (
+		dailyDataFrance.length
+			? (
+				<Line data={lineChartCasData} />
+			) : null
+	);
 
-    const lineChartDeathsData = {
-        labels: dailyDataFrance.map(({ date }) => date),
-        datasets: [{
-            data: dailyDataFrance.map(({ deces }) => deces),
-            label: 'Décédés',
-            borderColor: 'red',
-            backgroundColor: 'rgba(255,0,0,0.5)',
-            fill: true,
-            pointRadius: 0
-        }]
-    }
+	const lineChartDeathsData = {
+		labels: dailyDataFrance.map(({ date }) => date),
+		datasets: [{
+			data: dailyDataFrance.map(({ deces }) => deces),
+			label: 'Décédés',
+			borderColor: 'red',
+			backgroundColor: 'rgba(255,0,0,0.5)',
+			fill: true,
+			pointRadius: 0
+		}]
+	};
 
-    const lineChartDeaths = (
-        dailyDataFrance.length
-        ? (
-            <Line data={lineChartDeathsData} />
-        ) : null
-    );
+	const lineChartDeaths = (
+		dailyDataFrance.length
+			? (
+				<Line data={lineChartDeathsData} />
+			) : null
+	);
 
-    return (
-        <div className={styles.container}>
-            {lineChartCas}
-            {lineChartDeaths}
-        </div>
-    )
-}
+	return (
+		<div className={styles.container}>
+			{lineChartCas}
+			{lineChartDeaths}
+		</div>
+	);
+};
 
 ChartsFrance.propTypes = {
 	dailyDataFrance: PropTypes.isRequired
